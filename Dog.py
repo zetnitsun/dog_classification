d4fe6e49a333a3587570141dff207e0c4064c271
zetnitsun <108607064+zetnitsun@users.noreply.github.com>
2023-05-09 23:21:45 +0700

    Update Dog.py

--- Dog.py
+++ Dog.py
@@ -42,7 +42,8 @@ import urllib.request
 
 url = 'https://www.dropbox.com/s/k7c1qhddpidfxda/my_model_weights.h5?dl=1'
 
-urllib.request.urlretrieve(url, "brat.h5")
+if "brat.h5" is None:
+     urllib.request.urlretrieve(url, "brat.h5")
 # load weights into new model
 loaded_model.load_weights('brat.h5')
 
